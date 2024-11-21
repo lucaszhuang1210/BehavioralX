b8521f84374be36f6611744f7f6fa84328028c50
Kaixiang Zhuang <zhuangk2@uci.edu>
2024-11-20 16:22:38 -0800

    COMPLETED basic player movements
    Normalized player direction move

--- src/player.py
+++ src/player.py
@@ -34,9 +34,33 @@ class Player(pygame.sprite.Sprite):
             self.direction.x = 0
 
     def move(self, dt):
+        # Normalizing the vector
+        if self.direction.magnitude() > 0:
+            self.direction = self.direction.normalize()
+
+        # Horizontal Movement
+        self.pos.x += self.direction.x * self.speed * dt
+        self.rect.centerx = self.pos.x
+        
+        # Vertical Movement
+        self.pos.y += self.direction.y * self.speed * dt
+        self.rect.centery = self.pos.y
+
         self.pos += self.direction * self.speed * dt
         self.rect.center = self.pos
 
     def update(self, dt):
         self.input()
         self.move(dt)
+
+
+
+
+
+
+
+
+
+
+
+
